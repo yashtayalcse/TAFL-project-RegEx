b71d26d700de9d58c8aaade3a88f481455aa5516
yashtayalcse <yashtayal28763@gmail.com>
2026-04-07 17:49:52 +0530

    set up analytics

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './styles.css';
+import { Analytics } from "@vercel/analytics/react";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
+    <Analytics/>
   </React.StrictMode>,
 );
